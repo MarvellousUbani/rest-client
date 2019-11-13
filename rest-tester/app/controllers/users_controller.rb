e368a87ccf17f74490bc987980926e028a4add8e
Marvellous Michaels <marvellousubani@gmail.com>
2019-11-13 22:54:58 +0100

    added linter

--- rest-tester/app/controllers/users_controller.rb
+++ rest-tester/app/controllers/users_controller.rb
@@ -6,13 +6,29 @@ class UsersController < ApplicationController
 
  
   def show
+    @name = "I am the Show action!"
   end
 
   
   def new
-    @name = "I am the Index action!"
+    @name = "I am the New action!"
   end
 
   def edit
+    @name = "I am the Edit action!"
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        format.json { render :show, status: :created, location: @user }
+      else
+        format.html { render :new }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
   end
 end
--- rest-tester/app/script/rest_requestor.rb
+++ rest-tester/app/script/rest_requestor.rb
@@ -1,4 +1,5 @@
 require 'rest-client'
-url = "http://localhost:3000/users"
+url = "http://localhost:3000/users/"
 
-puts RestClient.get(url)
+# puts RestClient.get(url)
+puts RestClient.post(url,"")
